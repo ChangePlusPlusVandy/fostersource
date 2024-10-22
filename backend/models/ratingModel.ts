ec4945ee5c219f4249b3bbd93103fb9b6ffc20e4
ou-kevin <51072919+ou-kevin@users.noreply.github.com>
2024-10-22 17:29:34 -0500

    model and controller done

--- backend/models/ratingModel.ts
+++ backend/models/ratingModel.ts
@@ -1,14 +1,12 @@
 import mongoose, { Schema, Document, Model } from "mongoose";
 
 export interface IRating extends Document {
-  id: string;
   userId: string;
   rating: number;
 }
 
 const ratingSchema: Schema = new Schema(
   {
-    id: { type: String, required: true },
     userId: { type: String, required: true },
     rating: { type: Number, required: true }
   },
@@ -16,12 +14,6 @@ const ratingSchema: Schema = new Schema(
     timestamps: true, // Enable automatic createdAt and updatedAt fields
   }
 );
-
-// Define pre/post hooks or custom methods if necessary (optional)
-// ratingSchema.pre('save', function (next) {
-//   // Custom logic before saving the document
-//   next();
-// });
 
 const Rating: Model<IRating> = mongoose.model<IRating>(
   "Rating",
