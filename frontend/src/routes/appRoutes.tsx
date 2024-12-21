cccf37eb0790832181ecb0cfc3830fbcaba52117
Rachel Koh <rachelkohhuiqi@gmail.com>
2024-12-21 00:25:46 -0600

    gen: AppRoutes to show how components were displayed. Feel free to revert changes in case of merge conflict

--- frontend/src/routes/appRoutes.tsx
+++ frontend/src/routes/appRoutes.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Sidebar from "../components/sidebar";
+import HeaderBar from "../components/headerBar";
 
 const AppRoutes: React.FC = () => (
   <Router>
-    <div style={{ display: "flex", height: "100vh" }}>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <Sidebar />
-      </div>{" "}
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <div style={{ width: "100%" }}>
+        <HeaderBar />
+      </div>
+      <div style={{ display: "flex", flex: 1 }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Sidebar />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   </Router>
 );
