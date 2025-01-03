66128627b0746e43c2fc5b2cf4062e394700e94d
Ashrit-Anala <ashrit_ram@yahoo.com>
2025-01-03 17:59:12 -0600

    added new routes for new files

--- frontend/src/routes/appRoutes.tsx
+++ frontend/src/routes/appRoutes.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Catalog from "../pages/Catalog/Catalog"
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import ResetPassword from "../pages/resetPassword";
 
 const AppRoutes: React.FC = () => (
   <Router>
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
     </Routes>
   </Router>
 );
