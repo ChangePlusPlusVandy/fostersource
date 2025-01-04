e77bf73d0f71b1e64c4bd4672538778ac995f2da
Ashrit-Anala <ashrit_ram@yahoo.com>
2025-01-03 21:17:09 -0600

    added privateroute in app routes to redirect to login  if token not found

--- frontend/src/routes/appRoutes.tsx
+++ frontend/src/routes/appRoutes.tsx
@@ -1,21 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
-import Catalog from "../pages/Catalog/Catalog"
+import Catalog from "../pages/Catalog/Catalog";
 import Login from "../pages/UserAuth/Login";
 import Register from "../pages/UserAuth/Register";
 import ResetPassword from "../pages/UserAuth/resetPassword";
 import ResetPasswordForm from "../pages/UserAuth/resetPasswordForm";
 
+const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+  const token = localStorage.getItem("jwt");
+  if (token) {
+    return children;
+  } 
+  return <Navigate to="/login" />;
+};
+
 const AppRoutes: React.FC = () => (
   <Router>
     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPasswordForm />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/catalog" element={<PrivateRoute><Catalog /></PrivateRoute>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/reset-password/:token" element={<ResetPasswordForm />} />
     </Routes>
   </Router>
 );
