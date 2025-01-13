cf59ac1f2fa7f6287093ef47bcb58d1347de3321
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-01-12 21:37:26 -0600

    sidebar: sample use in appRoutes

--- frontend/src/routes/appRoutes.tsx
+++ frontend/src/routes/appRoutes.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-import Sidebar from "../components/sidebar";
+import { Sidebar } from "../components/sidebar";
 import HeaderBar from "../components/headerBar";
 
 const AppRoutes: React.FC = () => (
