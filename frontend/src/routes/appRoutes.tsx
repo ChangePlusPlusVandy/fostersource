3fff82776699a54c63d48a03046dda37bacd03f1
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-01-12 23:56:25 -0600

    v1: sidebar & header bar done! 🎉🎉🎉 see appRoutes for implementation. the code will not work without the states declared in appRoutes.tsx and the applied stylings

--- frontend/src/routes/appRoutes.tsx
+++ frontend/src/routes/appRoutes.tsx
@@ -1,25 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import { Sidebar } from "../components/sidebar";
-import HeaderBar from "../components/headerBar";
+import HeaderBar, { HeaderItems } from "../components/headerBar";
 
-const AppRoutes: React.FC = () => (
-  <Router>
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
-      <div style={{ width: "100%" }}>
-        <HeaderBar />
-      </div>
-      <div style={{ display: "flex", flex: 1 }}>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <Sidebar />
+function AppRoutes() {
+  const [isHeaderBarOpen, setIsHeaderBarOpen] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(
+    window.innerWidth < 768
+  );
+
+  return (
+    <Router>
+      <div
+        style={{ display: "flex", flexDirection: "column", height: "100vh" }}
+      >
+        <div style={{ width: "100%" }}>
+          <HeaderBar isOpen={isHeaderBarOpen} setIsOpen={setIsHeaderBarOpen} />
         </div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <div style={{ display: "flex", flex: 1 }}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Sidebar
+              isCollapsed={isCollapsed}
+              setIsCollapsed={setIsCollapsed}
+            />
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        {isHeaderBarOpen && isCollapsed && (
+          <div
+            className="p-7"
+            style={{
+              position: "absolute",
+              backgroundColor: "#f0f0f0",
+              top: "4rem",
+              bottom: 0,
+              left: 0,
+              right: 0,
+            }}
+          >
+            <HeaderItems
+              displayOptions="flex flex-col gap-3"
+              outline="border-black"
+            />
+          </div>
+        )}
       </div>
-    </div>
-  </Router>
-);
+    </Router>
+  );
+}
 
 export default AppRoutes;
