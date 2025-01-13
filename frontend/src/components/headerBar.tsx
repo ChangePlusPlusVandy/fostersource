c0cc3e5674ee4eb4c673e3531b80e4263738dda4
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-01-12 21:51:18 -0600

    header bar: added function to display header items

--- frontend/src/components/headerBar.tsx
+++ frontend/src/components/headerBar.tsx
@@ -1,13 +1,70 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./headerBar.css";
 
+export const headerItems = [
+  {
+    description: "Home",
+    href: "#",
+  },
+  {
+    description: "About",
+    href: "#",
+  },
+  {
+    description: "Programs",
+    href: "#",
+  },
+  {
+    description: "Calendar",
+    href: "#",
+  },
+  {
+    description: "How to Help",
+    href: "#",
+  },
+  {
+    description: "News",
+    href: "#",
+  },
+  {
+    description: "Contact",
+    href: "#",
+  },
+  {
+    description: "Portal",
+    href: "#",
+  },
+  {
+    description: "🇺🇸 English",
+    href: "#",
+  },
+];
+
+// The Header Bar
 const HeaderBar = () => {
   const [activeItem, setActiveItem] = useState<string>("");
 
   const handleItemClick = (item: string) => {
     setActiveItem(item);
   };
+
+  // State to handle collapsibility
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(
+    window.innerWidth < 768
+  );
+
+  // Automatically collapse header bar for narrow screens
+  useEffect(() => {
+    const handleResize = () => {
+      setIsCollapsed(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className="header flex flex-row w-full mb-3">
@@ -54,5 +111,16 @@ const HeaderBar = () => {
     </div>
   );
 };
+
+// Display and handle header bar entries
+export function HeaderItems() {
+  const headerItemList = headerItems.map(({ description, href }) => (
+    <li>
+      <Link to={href}>{description}</Link>
+    </li>
+  ));
+
+  return <ul className="header-menu">{headerItemList}</ul>;
+}
 
 export default HeaderBar;
