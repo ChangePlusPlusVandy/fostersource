9daf3c6e809485e9dc23c5bdab3af8452d156293
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-01-12 21:53:04 -0600

    header bar: abstracted out logo and items properly

--- frontend/src/components/headerBar.tsx
+++ frontend/src/components/headerBar.tsx
@@ -68,49 +68,22 @@ const HeaderBar = () => {
 
   return (
     <div className="header flex flex-row w-full mb-3">
-      <img
-        src="/assets/fostersource-logo.png"
-        alt="FosterSource logo"
-        className="header-image"
-      />
-      <ul className="header-menu">
-        <li className={activeItem === "home" ? "active" : ""}>
-          <Link to="/" onClick={() => handleItemClick("home")}>
-            Home
-          </Link>
-        </li>
-        <li className={activeItem === "about" ? "active" : ""}>
-          <Link to="/about" onClick={() => handleItemClick("about")}>
-            About
-          </Link>
-        </li>
-        <li className={activeItem === "programs" ? "active" : ""}>
-          <Link to="/programs" onClick={() => handleItemClick("programs")}>
-            Programs
-          </Link>
-        </li>
-        <li>
-          <Link to="/">Calendar</Link>
-        </li>
-        <li>
-          <Link to="/">How to help</Link>
-        </li>
-        <li>
-          <Link to="/">News</Link>
-        </li>
-        <li>
-          <Link to="/">Contact</Link>
-        </li>
-        <li>
-          <Link to="/">Portal</Link>
-        </li>
-        <li>
-          <Link to="/">🇺🇸 English</Link>
-        </li>
-      </ul>
+      <FosterSourceLogo />
+      <HeaderItems />
     </div>
   );
 };
+
+// Display the Fostersource logo
+export function FosterSourceLogo() {
+  return (
+    <img
+      src="/assets/fostersource-logo.png"
+      alt="FosterSource logo"
+      className="header-image"
+    />
+  );
+}
 
 // Display and handle header bar entries
 export function HeaderItems() {
