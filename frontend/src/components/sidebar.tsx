14a5ad12287029be27cbd851f18563fe64714f00
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-01-12 20:59:02 -0600

    sidebar: some progress, have conditional rendering of logout button but broke rendering of profile pic when logged in

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./sidebar.css";
 import {
@@ -11,49 +11,17 @@ import {
   ShoppingCart,
   Phone,
   LogOut,
+  LogIn,
 } from "lucide-react";
-import { isConditionalExpression } from "typescript";
 
-export function Sidebar() {
-  const user = "First L.";
-  const role = "Child";
+// TODO User information
+export const userInfo = {
+  name: "First L.",
+  role: "Role",
+  isLoggedIn: true,
+};
 
-  const [isCollapsed, setIsCollapsed] = useState<boolean>(
-    window.innerWidth < 768
-  );
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsCollapsed(window.innerWidth < 768);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  return (
-    <div className={`sidebar ${isCollapsed ? "collapsed" : ""} mr-3`}>
-      <div className="profile flex flex-row w-full">
-        <img
-          src="assets/cute_ghost.jpeg"
-          alt="Profile"
-          className="profile-pic"
-        />
-        {!isCollapsed && (
-          <div className="pl-3 align-middle">
-            <p className="text-xl font-medium text-wrap">{user}</p>
-            <p className="text-xs text-gray-600 mt-1">{role}</p>
-          </div>
-        )}
-      </div>
-
-      <SidebarItems />
-    </div>
-  );
-}
-
+// All sidebar entries
 export const items = [
   {
     icon: <House />,
@@ -87,21 +55,31 @@ export const items = [
   },
   { icon: <ShoppingCart />, description: "Cart", href: "#" },
   {
-    icon: <LogOut />,
-    description: "Logout",
+    icon: <Phone />,
+    description: "Contact",
     href: "#",
   },
 ];
 
-export function SidebarItems() {
-  const handleItemClick = (item: string) => {
-    setActiveItem(item);
-  };
+export const logout = {
+  icon: <LogOut />,
+  description: "Logout",
+  href: "#",
+};
 
+// The Sidebar itself
+export function Sidebar() {
+  // User Info
+  const name = userInfo.name;
+  const role = userInfo.role;
+  const isLoggedIn = userInfo.isLoggedIn;
+
+  // State to handle collapsibility
   const [isCollapsed, setIsCollapsed] = useState<boolean>(
     window.innerWidth < 768
   );
 
+  // Automatically collapse sidebar for narrow screens
   useEffect(() => {
     const handleResize = () => {
       setIsCollapsed(window.innerWidth < 768);
@@ -113,24 +91,93 @@ export function SidebarItems() {
     };
   }, []);
 
+  return (
+    <div className="sidebar">
+      <Profile
+        isCollapsed={isCollapsed}
+        isLoggedIn={isLoggedIn}
+        name={name}
+        role={role}
+      />
+      <SidebarItems isCollapsed={isCollapsed} isLoggedIn={isLoggedIn} />
+    </div>
+  );
+}
+
+interface ProfileProps {
+  isCollapsed: boolean;
+  isLoggedIn: boolean;
+  name?: string;
+  role?: string;
+}
+
+// Display profile picture
+export function Profile({ isCollapsed, isLoggedIn, name, role }: ProfileProps) {
+  return (
+    <div className="profile flex flex-row items-center w-full">
+      {!isLoggedIn && (
+        <div className="w-full flex justify-center">
+          <a href="#" className="w-full">
+            <button className="login text-white rounded p-3 flex gap-3 justify-center text-center w-full">
+              <LogIn /> {!isCollapsed && "Login"}
+            </button>
+          </a>
+        </div>
+      )}
+      {isLoggedIn && (
+          <img
+            src="assets/cute_ghost.jpeg"
+            alt="Profile"
+            className="profile-pic"
+          />
+        ) &&
+        !isCollapsed && (
+          <div className="pl-3 align-middle">
+            <p className="text-xl font-medium text-wrap">{name}</p>
+            <p className="text-xs text-gray-600 mt-1">{role}</p>
+          </div>
+        )}
+    </div>
+  );
+}
+
+interface SidebarItemsProps {
+  isLoggedIn: boolean;
+  isCollapsed: boolean;
+}
+
+// Display and handle sidebar entries
+export function SidebarItems({ isCollapsed, isLoggedIn }: SidebarItemsProps) {
+  // Helper function for active tab highlighting
+  const handleItemClick = (item: string) => {
+    setActiveItem(item);
+  };
+
   const [activeItem, setActiveItem] = useState<string>("");
 
   const sidebarItems = items.map(({ icon, description, href }) => {
     const active = activeItem === description ? "active" : "";
-    const logout = description === "Logout" ? "logout" : "";
+    const iconDescMargin = !isCollapsed ? "mr-4" : "";
 
     return (
-      <div className={`${logout}`}>
-        <li
-          className={`${active}`}
-          onClick={() => handleItemClick(description)}
-        >
-          {icon}
-          <Link to={href}>{description}</Link>
-        </li>
-      </div>
+      <li className={`${active}`} onClick={() => handleItemClick(description)}>
+        <div className={`${iconDescMargin}`}>{icon}</div>
+        <Link to={href}>{!isCollapsed && description}</Link>
+      </li>
     );
   });
 
-  return <ul className="menu mb-4">{sidebarItems}</ul>;
+  const iconDescMargin = !isCollapsed ? "mr-4" : "";
+
+  return (
+    <ul className="menu mb-4">
+      {sidebarItems}
+      {isLoggedIn && (
+        <li className="logout" onClick={() => handleItemClick("logout")}>
+          <div className={`${iconDescMargin}`}>{logout.icon}</div>
+          <Link to="#">{!isCollapsed && logout.description}</Link>
+        </li>
+      )}
+    </ul>
+  );
 }
