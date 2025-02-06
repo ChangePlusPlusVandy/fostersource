25494dc50131a6361f86ae5e6de22ad124bbc14e
Jonathan Kim <113380851+jkim-21@users.noreply.github.com>
2025-02-06 11:59:52 -0600

    Create model for handout feature

---
+++ backend/models/handoutModel.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface IHandout extends Document {
+    id: string;
+    courseId: string;
+    fileUrl: string;
+    fileType: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+  }
+
+  const handoutSchema: Schema = new Schema(
+    {
+        courseId: { type: String, required: true },
+        fileUrl: { type: String, required: true },
+        fileType: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+    }
+  );
+
+const Handout: Model<IHandout> = mongoose.model<IHandout>("Handout", handoutSchema);
+
+export default Handout;
