a1e4af3691b4ad7dffcb15df2b7fa2ede22d7c1b
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-02-07 21:50:25 -0600

    speaker model

---
+++ backend/models/speakerModel.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface ISpeaker extends Document {
+	name: string;
+	title: string;
+	email: string;
+	company: string;
+	bio: string;
+	disclosures?: string;
+	image: Object;
+}
+
+const SpeakerSchema: Schema = new Schema(
+	{
+		name: [{ type: String, required: true }],
+        title: [{ type: String, required: true }],
+        email: [{ type: String, required: true }],
+        company: [{ type: String, required: true }],
+        bio: [{ type: String, required: true }],
+        disclosures: [{ type: String, required: false }],
+        image: [{ type: Object, required: true }],
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Speaker: Model<ISpeaker> = mongoose.model<ISpeaker>("Speaker", SpeakerSchema);
+
+export default Speaker;
