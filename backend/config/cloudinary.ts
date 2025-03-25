cc582d9690469c7662f48f2f62e76a334bcc5cee
Yifei Fang <yifei.f1@student.fortbendisd.com>
2025-03-25 14:24:25 -0500

    oops i leaked the cloudniary api key

--- backend/config/cloudinary.ts
+++ backend/config/cloudinary.ts
@@ -3,12 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("cloudinary");
-
 cloudinary.config({
-	cloud_name: "dmv3souam",
-	api_key: "823766963339684",
-	api_secret: "KiUR4mPC15TBhRW9ZZ6FElLtUbQ",
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+	api_key: process.env.CLOUDINARY_API_KEY!,
+	api_secret: process.env.CLOUDINARY_API_SECRET!,
 });
 
 export default cloudinary;
