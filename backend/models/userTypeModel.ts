65fc3829d5a0a24105a08ba67ab35c73f3434526
Yifei Fang <yifei.f1@student.fortbendisd.com>
2025-05-06 16:57:49 -0500

    added prices to user types

--- backend/models/userTypeModel.ts
+++ backend/models/userTypeModel.ts
@@ -2,15 +2,20 @@
 import mongoose, { Schema, Document, Model } from "mongoose";
 
 export interface IUserType extends Document {
-  name: string;
+	name: string;
+	cost: number;
 }
 
 const userTypeSchema: Schema = new Schema(
-  {
-    name: { type: String, required: true, unique: true },
-  },
-  { timestamps: true }
+	{
+		name: { type: String, required: true, unique: true },
+		cost: { type: Number, required: true },
+	},
+	{ timestamps: true }
 );
 
-const UserType: Model<IUserType> = mongoose.model<IUserType>("UserType", userTypeSchema);
+const UserType: Model<IUserType> = mongoose.model<IUserType>(
+	"UserType",
+	userTypeSchema
+);
 export default UserType;
