3163191470a4b0693ee718206c9a8945d22fdc25
Yifei Fang <yifei.f1@student.fortbendisd.com>
2025-05-23 00:38:48 -0500

    build

--- backend/firebase/firebaseAdmin.ts
+++ backend/firebase/firebaseAdmin.ts
@@ -1,5 +1,9 @@
 import admin from "firebase-admin";
-import serviceAccount from "../serviceAccountKey.json";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!);
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
